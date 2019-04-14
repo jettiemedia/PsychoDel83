2c3549e8bd781c68af4e5bcd7dc7f72892322a25
dchang6 <42654029+dchang6@users.noreply.github.com>
2019-04-14 18:00:56 -0400

    Here ya go

---
+++ PsychoDelic/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    public int maxHealth = 5;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat fireRate;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    public virtual void Die()
+    {
+        //This will be overwritten
+        Debug.Log(transform.name + " died.");
+    }
+   
+}
---
+++ PsychoDelic/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WeaponManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Weapons newItem, Weapons oldItem)
+    {
+        if (newItem != null)
+        {
+            damage.AddModifier(newItem.damageModifier);
+            fireRate.AddModifier(newItem.fireModifier);
+        }
+
+        if (oldItem != null)
+        {
+            damage.RemoveModifier(oldItem.damageModifier);
+            fireRate.RemoveModifier(oldItem.fireModifier);
+        }
+
+    }
+}
